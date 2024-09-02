85eeb49daa2b39e4f73653d40038ae3b51dfb685
Mathmeh <fhvgggh.ru@gmail.com>
2024-09-02 14:49:22 +0300

    first screen and naviagtion added
    (cherry picked from commit 143a7923ea6bed24844ea4ad6cf73c82d177250e)

---
+++ app/src/main/java/com/example/compose_magazin/presentation/catalogScreen/catalogScreen.kt
@@ -0,0 +1,22 @@
+package com.example.compose_magazin.presentation.catalogScreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+@Composable
+fun CatalogScreen(navController: NavHostController) {
+    Surface(modifier = Modifier.fillMaxSize(), color = Color(0xFFF0F0F0)) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Catalog Screen", style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+}
