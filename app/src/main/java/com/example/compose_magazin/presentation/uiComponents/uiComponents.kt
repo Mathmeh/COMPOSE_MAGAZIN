241b3732276016e66d234d8a383c03e302f5a2e4
Mathmeh <fhvgggh.ru@gmail.com>
2024-09-03 11:40:05 +0300

    minor changes

--- app/src/main/java/com/example/compose_magazin/presentation/uiComponents/uiComponents.kt
+++ app/src/main/java/com/example/compose_magazin/presentation/uiComponents/uiComponents.kt
@@ -1,14 +1,17 @@
 package com.example.compose_magazin.presentation.uiComponents
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ChangeProductAmountButton(
@@ -19,12 +22,14 @@ fun ChangeProductAmountButton(
     Button(
         onClick = onClickLambda,
         shape = CutCornerShape(8.dp),
-        modifier = Modifier.size(size)
-
+        modifier = Modifier.size(size),
+        contentPadding = PaddingValues(4.dp)
     ) {
         Text(
             text = buttonText,
-            modifier = Modifier.fillMaxSize(),
+            fontSize = 20.sp,
+            color = Color.Cyan,
+            modifier = Modifier.wrapContentSize()
         )
     }
 }
